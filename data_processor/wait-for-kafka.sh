f9b054e18c37f92f517d6f0a918d6e47a280c6bb
MrandrewGR <MrandrewGR@users.noreply.github.com>
2024-12-15 23:14:08 +0300

    fix

--- data_processor/wait-for-kafka.sh
+++ data_processor/wait-for-kafka.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 # wait-for-kafka.sh
 
-set -e
+HOST=$1
+PORT=$2
 
-host="$1"
-port="$2"
-shift 2
-cmd="$@"
+echo "Waiting for Kafka at $HOST:$PORT..."
 
-echo "Waiting for Kafka at $host:$port..."
-
-while ! nc -z "$host" "$port"; do
+while ! nc -z $HOST $PORT; do
   echo "Kafka is unavailable - sleeping"
-  sleep 1
+  sleep 5
 done
 
 echo "Kafka is up - executing command"
-exec $cmd
+exec "$@"

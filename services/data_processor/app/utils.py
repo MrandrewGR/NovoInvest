931a39abffde96c25e20e5aba0536a04eb13edaa
MrandrewGR <MrandrewGR@users.noreply.github.com>
2024-12-29 14:09:02 +0300

    удалил неиспользуемые микросеврисы

--- services/data_processor/app/utils.py
+++
@@ -1,9 +0,0 @@
-# File: services/data_processor/app/utils.py
-import os
-
-def ensure_dir(directory):
-    """
-    Создаёт директорию, если она не существует.
-    """
-    if not os.path.exists(directory):
-        os.makedirs(directory, exist_ok=True)

387746ce9a6e0df41d2f1eec9458059d352f0af1
MrandrewGR <MrandrewGR@users.noreply.github.com>
2024-12-30 04:14:21 +0300

    fix

--- services/bot/wait-for-services.sh
+++ services/bot/wait-for-services.sh
@@ -1,8 +1,23 @@
-# Перейдите в директорию бота
-cd ./services/bot
+#!/bin/bash
+set -e
 
-# Скачайте wait-for-it.sh
-curl -o wait-for-it.sh https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
+cd /app
 
-# Сделайте скрипт исполняемым
-chmod +x wait-for-it.sh
+wait_for_service() {
+  local host=$1
+  local port=$2
+  echo "Ожидание $host:$port..."
+  while ! nc -z "$host" "$port"; do
+    echo "$host недоступен - ожидание"
+    sleep 1
+  done
+  echo "$host доступен - продолжаем"
+}
+
+# Ожидание PostgreSQL (host=postgres, port=5432)
+wait_for_service postgres 5432
+
+# Ожидание Kafka (host=kafka, port=9092)
+wait_for_service kafka 9092
+
+exec "$@"

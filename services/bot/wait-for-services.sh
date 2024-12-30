4bfc58185bdfd83eed98908d61dbcfe072baccfe
MrandrewGR <MrandrewGR@users.noreply.github.com>
2024-12-30 03:58:21 +0300

    fix

--- services/bot/wait-for-services.sh
+++ services/bot/wait-for-services.sh
@@ -1,7 +1,8 @@
-# File location: ./services/file_processor/wait-for-services.sh
-#!/usr/bin/env bash
+# Перейдите в директорию бота
+cd ./services/tg_file_bot
 
-# Ждём запуска Kafka
-echo "Waiting for Kafka to be ready..."
-/wait-for-it.sh kafka:9092 --timeout=60 --strict -- echo "Kafka is up"
-exec "$@"
+# Скачайте wait-for-it.sh
+curl -o wait-for-it.sh https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
+
+# Сделайте скрипт исполняемым
+chmod +x wait-for-it.sh

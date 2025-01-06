a38ed44b010aa1a863fba7a42014dde5c4392649
MrandrewGR <MrandrewGR@users.noreply.github.com>
2025-01-06 08:23:23 +0300

    Update env.py

--- services/db/migrations/env.py
+++ services/db/migrations/env.py
@@ -4,6 +4,8 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config, pool
 from alembic import context
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from db.models import Base  # <-- наши модели
 
 # Подтягиваем настройки из alembic.ini
